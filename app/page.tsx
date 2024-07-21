93b4aaca274bae5be2823df3f31916297e9c70cb
Parthit Patel <pbp38@cornell.edu>
2024-07-20 22:40:01 -0400

    fixef typo

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        Parthits Personal Weboage
+        Parthits Personal Webpage
       </div>
     </main>
   );
